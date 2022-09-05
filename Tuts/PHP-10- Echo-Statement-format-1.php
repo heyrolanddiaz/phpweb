0b3000bfa4bb70731c81b9951925717815c0216c
Roland Diaz <iamrolanddiaz@gmail.com>
2022-09-06 07:18:15 +0800

    Updates-10
    Updates-10
    
    This is about the echo string statement formats

---
+++ Tuts/PHP-10- Echo-Statement-format-1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+
+<?php
+
+echo "<h2>PHP is Fun!</h2>"; // This is using HTML tags inside the echo statement. 
+echo "Hello World!<br>"; // This is usinh normal text inside the echo statement. 
+echo "I'm about to learn PHP!<br>"; // This is using html break tag inside the echo statement. 
+echo "This ", "string ", "was ", "made ", "with ", "multiple parameters."; //This is using double quotes and comma inside the echo statement. 
+
+?>
+
+</body>
+</html> 
