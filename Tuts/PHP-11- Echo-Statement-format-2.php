81f34508fd402098bcf049c542705675ffa10821
Roland Diaz <iamrolanddiaz@gmail.com>
2022-09-06 07:20:44 +0800

    Updates-11
    Updates-11
    
    This is about the combined echo statement format for string and integer.

---
+++ Tuts/PHP-11- Echo-Statement-format-2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html> 
+<html>
+<head></head>
+<body>
+<?php
+
+$txt1 = "Learn PHP"; // Always put double quotes for string variable.
+$txt2 = "iamrolanddiaz.wordpress.com"; 
+$x = 5; // No double quotes for integer number 
+$y = 4; // No double quotes for integer number
+
+echo "<h2>" . $txt1 . "</h2>"; // Always put double quotes for string variable. // Always put double quotes for string variable and no double quotes on integer number. 
+echo "Study PHP at " . $txt2 . "<br>"; // Always put double quotes for string variable and no double quotes on integer number. 
+echo $x = $y; // No double quotes for integer number
+
+?>
+</body>
+</html>
