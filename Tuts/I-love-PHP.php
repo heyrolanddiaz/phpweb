07017fae05c08d08731459d973e59931229789cb
Roland Diaz <iamrolanddiaz@gmail.com>
2022-09-05 12:52:07 +0800

    Updates-2
    Updates-2

---
+++ Tuts/I-love-PHP.php
@@ -0,0 +1,15 @@
+<html> 
+<body>
+
+<?php
+
+// This is a PHP variable 
+$txt = "PHP!";
+
+// This is a PHP Funtion to show or print any type of data on the browser. 
+echo "I LOVE $txt";
+
+?>
+
+</body> 
+</html> 
