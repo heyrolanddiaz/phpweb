2ed60164ecaeee209e84151a0edbe687c8dd52e6
Roland Diaz <iamrolanddiaz@gmail.com>
2022-09-05 13:02:10 +0800

    Updates-4
    Updates-4
    
    This is about PHP valid single line comments syntax.

---
+++ Tuts/Single-line-PHP-comment.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php 
+
+// This is a single-line comment 
+
+echo "// This is a single-line PHP comment! <br>"; 
+echo "# This is also single-line PHP commnet!";
+
+?>
+
+
+</body>
+</html>
